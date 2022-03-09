930106e384d20365fe534eda5c807b536e5decc1
Eitan Stadtlander-Miller <yodaissmart@gmail.com>
2022-03-09 00:16:27 -0500

    added hamburger

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,9 @@ import {
   ChartBarIcon,
   CogIcon,
   InformationCircleIcon,
+  MenuIcon
 } from '@heroicons/react/outline'
+import { useState } from 'react'
 import { GAME_TITLE } from '../../constants/strings'
 
 type Props = {
@@ -16,13 +18,17 @@ export const Navbar = ({
   setIsStatsModalOpen,
   setIsSettingsModalOpen,
 }: Props) => {
+  const [hamburgerOpen, setHamburgerOpen] = useState(false)
   return (
     <div className="navbar">
       <div className="navbar-content px-5">
-        <InformationCircleIcon
-          className="h-6 w-6 mr-2 cursor-pointer dark:stroke-white"
-          onClick={() => setIsInfoModalOpen(true)}
-        />
+        <div className='flex'>
+          <MenuIcon className='h-6 w-6 mr-3 cursor-pointer dark:stroke-white' onClick={() => setHamburgerOpen(val => !val)} />
+          <InformationCircleIcon
+            className="h-6 w-6 cursor-pointer dark:stroke-white"
+            onClick={() => setIsInfoModalOpen(true)}
+          />
+        </div>
         <p className="text-xl ml-2.5 font-bold dark:text-white">{GAME_TITLE}</p>
         <div className="right-icons">
           <ChartBarIcon
@@ -36,6 +42,11 @@ export const Navbar = ({
         </div>
       </div>
       <hr></hr>
+      {hamburgerOpen && <ol className="absolute left-0 top-12 p-4 gap-4 flex flex-col">
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ol>}
     </div>
   )
 }
